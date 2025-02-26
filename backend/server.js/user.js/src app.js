2a6b8f38dfc6e443fb5af83d3c7822483762efb7
saiamarnath2003 <sairameshkrishnaj@gmail.com>
2025-02-26 11:44:19 +0530

    Create src app.js

---
+++ backend/server.js/user.js/src app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserTable from './UserTable';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Management System</h1>
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
